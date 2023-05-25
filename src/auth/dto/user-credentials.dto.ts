2755632e8604a05a018c8b11afae1ffa916219d1
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-05-25 15:29:00 +0300

    password validation

--- src/auth/dto/user-credentials.dto.ts
+++ src/auth/dto/user-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!$\/%@])[A-Za-z\d!$\/%@]{6,20}$/,
+    { message: 'password is too week' },
+  )
   password: string;
 }
