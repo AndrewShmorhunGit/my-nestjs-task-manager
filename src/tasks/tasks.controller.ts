dc6a534cd369a96abe471e531d979c23bde6dc95
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-06-05 14:06:34 +0300

    fix dev - prod dynamic config. Bag with .env var

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,12 +18,18 @@ import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../auth/user.entity';
 import { GetUser } from '../auth/get-user.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController');
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private configService: ConfigService,
+  ) {
+    console.log(configService.get('TEST_VALUE'));
+  }
 
   @Get()
   getTasks(
